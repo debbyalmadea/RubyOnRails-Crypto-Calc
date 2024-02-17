b0a68072129d1be7dbeca4e56e803d9747c012f1
Altair1618 <103034064+Altair1618@users.noreply.github.com>
2024-02-17 14:08:39 +0700

    Feat/affine cipher (#4)
    * feat: implement affine cipher service
    
    * test: add test cases for affine cipher

---
+++ app/services/affine_cipher_service.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+# Service for encrypting and decrypting Affine Cipher
+class AffineCipherService
+  # param plain_text [String]
+  # param m [Integer]
+  # param b [Integer]
+  # return [String]
+  def encrypt(plain_text, m, b)
+    raise ArgumentError, 'm and 26 are not coprime' if m.gcd(26) != 1
+
+    plain_text = preprocess_text(plain_text)
+
+    cipher_text = ''
+    plain_text.each_char do |char|
+      cipher_text += encrypt_char(char, m, b)
+    end
+
+    cipher_text
+  end
+
+  # param cipher_text [String]
+  # param m [Integer]
+  # param b [Integer]
+  # return [String]
+  def decrypt(cipher_text, m, b)
+    raise ArgumentError, 'm and 26 are not coprime' if m.gcd(26) != 1
+
+    cipher_text = preprocess_text(cipher_text)
+
+    deciphered_text = ''
+    cipher_text.each_char do |char|
+      deciphered_text += decrypt_char(char, m, b)
+    end
+
+    deciphered_text
+  end
+
+  private
+
+  # @param char [String]
+  # @param m [Integer]
+  # @param b [Integer]
+  # @return [String]
+  def encrypt_char(char, m, b)
+    ((m * (char.ord - 'A'.ord) + b) % 26 + 'A'.ord).chr
+  end
+
+  # @param char [String]
+  # @param m [Integer]
+  # @param b [Integer]
+  # @return [String]
+  def decrypt_char(char, m, b)
+    ((mod_inverse(m, 26) * (char.ord - 'A'.ord - b)) % 26 + 'A'.ord).chr
+  end
+
+  # @param plain_text [String]
+  # @return [String]
+  def preprocess_text(plain_text)
+    plain_text.upcase.gsub(/[^A-Z]/, '')
+  end
+
+  # @param num [Integer]
+  # @param mod [Integer]
+  # @return [Integer]
+  def mod_inverse(num, mod)
+    (1..mod).each do |i|
+      return i if (num * i) % mod == 1
+    end
+  end
+end
