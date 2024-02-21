04e8a8cfcbf1ba6f47662460da3642ef1232b46f
debbyalmadea <almadeaputri@gmail.com>
2024-02-21 19:16:49 +0700

    fix: transpose and super cipher and cipher form max file size

--- app/services/transpose_cipher_service.rb
+++ app/services/transpose_cipher_service.rb
@@ -8,33 +8,26 @@ class TransposeCipherService
     def encrypt(plain_text, key)
       # puts plain_text
       matrix, remainder_array = preprocess_text(plain_text, key.length)
-
-      # puts "matriks #{matrix}"
-      # puts "remainder array #{remainder_array}"
       
       matrix = matrix.transpose
-      # puts "transpose #{matrix}"
 
       if remainder_array
         remainder_array.each_with_index do |c, i|
           matrix[i] << c
         end
-      end
 
-      # puts "concat remainder #{matrix}"
+        (key.length - remainder_array.length).times do |i|
+          matrix[remainder_array.length + i] << 0.chr
+        end
+      end
 
       order = preprocess_key(key)
 
-      # puts "order #{order}" 
-
       cipher_text = ''
 
-      # read the matrix row by row based on the order of the key
       order.each do |i|
         cipher_text += matrix[i].join('')
       end
-
-      # puts "cipher text #{cipher_text}"
 
       cipher_text
     end
@@ -50,34 +43,15 @@ class TransposeCipherService
       order = preprocess_key(key)
       order = key.length.times.map { |i| order.index(i) }
 
-      # puts "order #{order}"
-
-      (cipher_text.length % key.length).times do |i|
-        remainder_char_idx = len * order[i] + len
-        remainder_array << cipher_text[remainder_char_idx]
-        cipher_text = cipher_text[0...remainder_char_idx] + cipher_text[remainder_char_idx + 1..-1]
-      end
-
-      # puts "remainder array #{remainder_array}"
-
       matrix, _ = preprocess_text(cipher_text, len)
 
-      # puts "matriks #{matrix}"
-
-      # reorder the matrix based on the order of the key
       matrix = order.map { |i| matrix[i] }.transpose
-
-      # puts "reorder #{matrix}"
 
       if remainder_array
         matrix << remainder_array
       end
 
-      # puts "concat remainder #{matrix}"
-
       deciphered_text = matrix.join('')
-
-      # puts "deciphered text #{deciphered_text}"
 
       deciphered_text
     end
