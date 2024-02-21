3d07822a0b1ac203f3343a2e9ecd2512aaadc60e
debbyalmadea <almadeaputri@gmail.com>
2024-02-21 19:15:51 +0700

    fix: playfair cipher gsub J to I

--- app/services/playfair_cipher_service.rb
+++ app/services/playfair_cipher_service.rb
@@ -8,7 +8,7 @@ class PlayfairCipherService
     def encrypt(plain_text, matrix)
       bigram_array = preprocess_text(plain_text)
       cipher_text = ''
-      bigram_array.each do |bigram|
+      bigram_array.each.with_index do |bigram|
         cipher_text += encrypt_bigram(bigram, matrix)
       end
 
@@ -45,11 +45,11 @@ class PlayfairCipherService
         x2, y2 = find_char_pos(bigram[1], matrix)
 
         if x1 == x2 # same row
-            return matrix[x1][(y1 + 1) % 5] + matrix[x2][(y2 + 1) % 5]
+          return matrix[x1][(y1 + 1) % 5] + matrix[x2][(y2 + 1) % 5]
         elsif y1 == y2 # same column
-            return matrix[(x1 + 1) % 5][y1] + matrix[(x2 + 1) % 5][y2]
+          return matrix[(x1 + 1) % 5][y1] + matrix[(x2 + 1) % 5][y2]
         else
-            return matrix[x1][y2] + matrix[x2][y1]
+          return matrix[x1][y2] + matrix[x2][y1]
         end
     end
   
@@ -81,8 +81,8 @@ class PlayfairCipherService
     def preprocess_text(plain_text)
       # change j to i
       plain_text = plain_text.upcase.gsub(/[^A-Z]/, '')
-      plain_text.gsub('J', 'I')
-
+      plain_text = plain_text.gsub('J', 'I')
+      
       # insert X in between same bigram
       plain_text = plain_text.gsub(/(.)(?=\1)/, '\1X')
 
